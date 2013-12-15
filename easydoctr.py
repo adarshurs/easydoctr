48a8fc5d342e0b1d39ebd78307c572bbfbee0793
adarshurs <adarshrajurs@gmail.com>
2013-12-15 18:59:25 +0530

    Message box, send message functionality implemented.

--- easydoctr.py
+++ easydoctr.py
@@ -3,17 +3,18 @@ import urllib
 import webapp2
 import cgi
 import json
+import requests
 from urlparse import urlparse
 from google.appengine.ext import ndb
 from google.appengine.ext.webapp import template
-#from google.appengine.api import mail
+from google.appengine.api import mail
 
 patients = ""
 all_emails = ""
 
 
 class Message(ndb.Model):
-	Id = ndb.StringProperty() 
+	Mail_Id = ndb.StringProperty() 
 	To = ndb.StringProperty()
 	From = ndb.StringProperty()
 	Cc = ndb.StringProperty()
@@ -48,17 +49,45 @@ class SavePatientData:
 					p.Mails.append(mail)
 					p.put()
 
+
+# def send_simple_message():
+#     return requests.post(
+#         "https://api.mailgun.net/v2/samples.mailgun.org/messages",
+#         auth=("api", "key-484575zwsy10v23tqqc5droje97r-7n8"),
+#         data={"from": "Excited User <postmaster@sandbox3948.mailgun.org>",
+#               "to": ["adarshrajurs@gmail.com"],
+#               "subject": "Hello",
+#               "text": "Testing some Mailgun awesomeness!"})
+
+def send_mail_from_gmail(mail):
+	user = users.get_current_user()
+	message = mail.EmailMessage()
+	message.sender = user.email()
+	message.to = mail.to
+	message.body = mail.body
+	message.send()
+
+
+
 class MainPage(webapp2.RequestHandler):
-	def get(self):				
+	def get(self):
+		user = users.get_current_user()
+		if user is None:
+			login_url = users.create_login_url(self.request.path)
+			self.redirect(login_url)
+			return
+
+		#send_mail_from_gmail()
+
 		sock = urllib.urlopen("https://patients.apiary.io/patients")
 		data = json.loads(sock.read())
 		sock.close()
 		strpatient = SavePatientData()
 
-		# mail1 = Message(To = "abhinav@practo.com",From="thisDoctor@practo.com",Cc="",Subject="Next week I'll not be available",Message_Content="Hi Everyone, \n Next week I'm going for a vacation. So those who have check up schedules for next week, please visit me after next week. \n For any emergencies you can contact the clinic at any time.\n  Thank You \n Doctor")
+		# mail1 = Message(Mail_Id = "abhinav@practo.com",To = "abhinav@practo.com",From="thisDoctor@practo.com",Cc="",Subject="Next week I'll not be available",Message_Content="Hi Everyone, \n Next week I'm going for a vacation. So those who have check up schedules for next week, please visit me after next week. \n For any emergencies you can contact the clinic at any time.\n  Thank You \n Doctor")
 		# mail1.put()
 
-		# mail2 = Message(To = "praveen@practo.com",From="thisDoctor@practo.com",Cc="",Subject="Your this month check up is scheduled to",Message_Content="Hi Everyone, \n Next week I'm going for a vacation. So those who have check up schedules for next week, please visit me after next week. \n For any emergencies you can contact the clinic at any time.\n  Thank You \n Doctor")
+		# mail2 = Message(Mail_Id = "praveen@practo.com",To = "praveen@practo.com",From="thisDoctor@practo.com",Cc="",Subject="Your this month check up is scheduled to",Message_Content="Hi Everyone, \n Next week I'm going for a vacation. So those who have check up schedules for next week, please visit me after next week. \n For any emergencies you can contact the clinic at any time.\n  Thank You \n Doctor")
 		# mail2.put()
 
 
@@ -76,7 +105,8 @@ class MainPage(webapp2.RequestHandler):
 
 		# for p in patients:
 		# 	p.key.delete()
-		patients = Patient.query()
+		#patients = Patient.query()
+		
 
 		all_emails = Message.query()
 
@@ -96,21 +126,37 @@ class MainPage(webapp2.RequestHandler):
 			'all_mails' : all_emails			
 		}
 
-		#self.response.write(mail1)
+		#self.response.write(response)
 		self.response.write(template.render("index.html",template_values))
 
-class Save(webapp2.RequestHandler):	
+class Send(webapp2.RequestHandler):	
 	def post(self):
-		ids = self.request.get['selectedPatientsIds']
-		message = self.request.get['message']
-		strpatient = SavePatientData()
-		strpatient.updatePatientMail(ids)
+		#mail_to_send = {'To':[],'subject':"",'text':""}
+		To = self.request.get('to')
+		subject = self.request.get('subject')
+		text = self.request.get('text')
 
-		
+		self.response.write(subject);
+		# strpatient = SavePatientData()
+		# strpatient.updatePatientMail(ids)
+		#self.redirect('/?')
 
+class FilterMessages(webapp2.RequestHandler):
+	def post(self):
+		mail_id = self.request.POST["email"]
+		if mail_id:
+			MessagesToShow = Message.query(Message.Mail_Id == mail_id).fetch(10)
+			all_emails = MessagesToShow
 
-		self.redirect('/?')
+		#all_emails = Message.query()
+		patients = Patient.query()
+		template_values = {
+			'patients':patients,
+			'all_mails' : MessagesToShow			
+		}
+
+		self.response.write(template.render("index.html",template_values))
 		
 	
 application = webapp2.WSGIApplication([
-		('/',MainPage),('/save',Save)], debug=True)
+		('/',MainPage),('/sendMessage',Send),('/getMessages',FilterMessages)], debug=True)
